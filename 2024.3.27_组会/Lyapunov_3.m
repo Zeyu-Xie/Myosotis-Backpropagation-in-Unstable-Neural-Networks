f058a9ed5af5917ae1b4d64589857db7de27fb42
Acan <xie.zeyu20@gmail.com>
2024-03-26 21:32:37 +0800

    .

--- 2024.3.27_组会/Lyapunov_3.m
+++ 2024.3.27_组会/Lyapunov_3.m
@@ -1,4 +1,4 @@
-% 数值微分求 Jacobi 矩阵，然后算 Lyapunov 指数
+% 数值微分求 Jacobi 矩阵，然后算 Lyapunov 谱
 
 f = @(x) 1.145*[x(1)*cos(pi/9)-x(2)*sin(pi/9); x(1)*sin(pi/9)+x(2)*cos(pi/9)];
 
@@ -21,15 +21,14 @@ format long;
 
 for i = 2:90
     J_tmp = J(:,2*i-1:2*i);
-    J_tmp = log(det(J_tmp))/(i-1);
-    Lyapunov = [Lyapunov, J_tmp];
+    J_tmp = log(abs(eig(J_tmp)))/(i-1);
     disp(J_tmp);
+    Lyapunov = [Lyapunov, max(abs(J_tmp(1)), abs(J_tmp(2)))];
 end
 
-% 绘制 Lyapunov 指数随迭代次数的变化图表
-iterations = 1:89;
-plot(iterations, Lyapunov);
-xlabel('迭代次数');
-ylabel('Lyapunov 指数');
-title('Lyapunov 指数随迭代次数的变化');
+plot(1:89, Lyapunov, 'o-', 'LineWidth', 2, "DisplayName", "Lyapunov Exponent");
+xlabel('n');
+ylabel('Lyapunov Exponent');
+title("Lyapunov Exponent");
+legend('show');
 grid on;
